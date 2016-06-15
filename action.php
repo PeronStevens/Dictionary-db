440b528555c1e4c44385ff412f08b7455dbc1de1
Peron Stevens <PeronStevens@yahoo.com>
2016-06-14 20:46:07 -0400

    Restructure

--- action.php
+++ action.php
@@ -1,23 +1,54 @@
-You entered
-
  <?php  
- 
-       $var = $_REQUEST["q"];
-       echo $var;
-       
+
+      $var = $_REQUEST["q"];
+              
+      $q = "select definition, wordtype from entries where word  = '$var'";
+              
       $servername = "localhost";
       $username = "root";
       $password = "password";
     
-      $conn = mysql_connect($servername, $username, $password);
-        
-      // Check connection
-      if (!$conn) {
-          die("Connection failed: " . mysqli_connect_error());
-      }
-      echo "<br />Connected to MySQL successfully";
+      connect($servername, $username, $password);
+      //query($q);  
+      display(query($q));   
+
       //Close connection
       mysql_close();
+
+      //Connect to SQL database
+      function connect($servername, $username, $password){
+
+            $conn = mysql_connect($servername, $username, $password);
+            
+            // Check connection
+            if (!$conn) {
+                die("Connection failed: " . mysql_error());
+            }
+      }
+      //Select table and query database
+      function query($q){
+
+            mysql_select_db(entries) 
+                or die("NO SUCH DB");
+
+            $r = mysql_query($q);
+
+            if (mysql_num_rows($r) > 0)
+                $a = mysql_fetch_assoc($r);
+
+            return $a;
+      }
+
+      //Access array and display fields
+      function display($a){
+
+
+        echo '<i>'.$a['wordtype'].'</i>'.'<br /><br />'.$a['definition'];
+
+        if ($a['definition'] == NULL)
+            echo "word undefined";
+
+      }
   ?>
   
  
