4ceae917422483c413dd6bc6f0cabd866c1b71e0
Peron Stevens <PeronStevens@outlook.com>
2017-01-03 09:44:17 -0500

    Change database to SQLite

--- action.php
+++ action.php
@@ -1,32 +1,14 @@
  <?php  
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = 'password';
-    $db_name = 'entries';
-
-    try {
-        $entries = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
-    } catch (PDOException $e){
-        echo 'Error: ' . $e->getMessage(); 
-    }
-
-    function db_query($database, $string, $values = NULL) {
-        $temp_query = $database->prepare($string);
-        if (is_null($values)) {
-            $temp_query->execute();
-        } else {
-            $temp_query->execute($values);
-        }
-        return $temp_array = $temp_query->fetchAll(PDO::FETCH_ASSOC);
-    }
-
     $word = $_POST['word'];
 
-    $res = db_query($entries, 'SELECT definition, wordtype FROM entries WHERE word = ?', [$word]);
+    $db = new SQLite3('dict.db');
 
-    if (isset($res[0])){
-        echo '<i>'.$res[0]['wordtype'].'</i>'.'<br /><br />'.$res[0]['definition'];
+    $temp = $db->query("SELECT * FROM dict WHERE word = '$word' COLLATE NOCASE");
+    $res  = $temp->fetchArray();
+
+    if (isset($res)){
+        echo '<i>'.$res['wordtype'].'</i>'.'<br /><br />'.$res['definition'];
     } else {
         echo "word undefined";
     }
